19b113f90a91e4dadbd993fe9ada3c56805cf03a
workingdog <ringowathelet@gmail.com>
2017-10-14 17:14:37 +0900

    initial commit

--- src/server/collections.js
+++ src/server/collections.js
@@ -3,19 +3,17 @@
 
 // @flow weak
 
-import {Collections, Collection} from '../libs/taxii2lib.js';
+import {Collections} from '../libs/taxii2lib.js';
 import Grid from 'material-ui/Grid';
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Typography from 'material-ui/Typography';
-import List, {ListItemText} from 'material-ui/List';
 import {FormControlLabel} from 'material-ui/Form';
 import Radio, {RadioGroup} from 'material-ui/Radio';
 import green from 'material-ui/colors/green';
 import red from 'material-ui/colors/red';
 import {CircularProgress} from 'material-ui/Progress';
-
-
+import Table, {TableBody, TableCell, TableRow} from 'material-ui/Table';
 
 const styles = theme => ({
     root: {
@@ -46,7 +44,8 @@ export class CollectionsPage extends Component {
             selectedColid: '',
             collectionList: [],
             objectList: [],
-            apiroot: ''
+            apiroot: '',
+            info: ''
         };
     }
 
@@ -55,40 +54,15 @@ export class CollectionsPage extends Component {
         if (theServer && apiroot) {
             let colInfo = JSON.parse(localStorage.getItem('collectionSelected'));
             let colid = colInfo ? colInfo.id : '';
-            //    this.setState({apiroot: apiroot, selectedColid: colid});
-            this.state.apiroot = apiroot;
-            this.state.selectedColid = colid;
-            this.forceUpdate();
-            // if already have a collection id selected
-            if (colid) {
-                this.setState({waiting: true});
-                const theCollections = new Collections(apiroot, theServer.conn);
-                theCollections.get().then(collections => {
-                    let colList = [];
-                    // fill the array with collections info
-                    collections.map(col => colList.push(col));
-                    // get the selected collection info
-                    let thisCol = colList.find(col => col.id === colid);
-                    // create the selected collection endpoint
-                    const theCollection = new Collection(thisCol, apiroot, theServer.conn);
-                    // get the objects
-                    theCollection.getObjects().then(bundle => {
-                        this.setState({collectionList: colList, objectList: bundle.objects, waiting: false});
-                        this.forceUpdate();
-                    });
-                });
-            } else {
-                // just list the collections
-                this.setState({waiting: true});
-                const theCollections = new Collections(apiroot, theServer.conn);
-                theCollections.get().then(collections => {
-                    let colList = [];
-                    collections.map(col => colList.push(col));
-                    this.setState({collectionList: colList, waiting: false});
-                });
-            }
+            this.setState({info: colInfo, apiroot: apiroot, selectedColid: colid, waiting: true});
+            const theCollections = new Collections(apiroot, theServer.conn);
+            theCollections.get().then(collections => {
+                let colList = [];
+                collections.map(col => colList.push(col));
+                this.setState({collectionList: colList, waiting: false});
+            });
         } else {
-            this.setState({waiting: false, selectedColid: '', collectionList: [], objectList: [], apiroot: ''});
+            this.setState({waiting: false, selectedColid: '', collectionList: [], objectList: [], apiroot: '', info: ''});
         }
     };
 
@@ -102,22 +76,9 @@ export class CollectionsPage extends Component {
         this.initialise(newProps.server);
     };
 
-    // get the objects of the selected collection
-    dataObjectList(col) {
-        if (this.state.apiroot && this.props.server) {
-            this.setState({waiting: true});
-            const theCollection = new Collection(col, this.state.apiroot, this.props.server.conn);
-            theCollection.getObjects().then(bundle =>
-                this.setState({objectList: bundle.objects, waiting: false}));
-        }
-    };
-
     colsAsFormLabels() {
         let colItems = [];
         this.state.collectionList.map(col => {
-            let readVal = col.can_read ? 'can read' : 'cannot read';
-            let writeVal = col.can_write ? 'can write' : 'cannot write';
-            let labelValue = col.title + ' (' + readVal + ', ' + writeVal + ')';
             let theColor = (col.can_write && col.can_read) ? green[500] : red[500];
             colItems.push(<FormControlLabel style={{margin: 8}}
                                             disabled={!col.can_read}
@@ -125,33 +86,58 @@ export class CollectionsPage extends Component {
                                             value={col.id}
                                             checked={col.id === this.state.selectedColid}
                                             control={<Radio style={{color: theColor}}/>}
-                                            label={labelValue}/>);
+                                            label={col.title}/>);
         });
         return colItems;
-    }
-
-    objsAsFormLabels() {
-        let objItems = [];
-        this.state.objectList.map(sdo => {
-            objItems.push(<ListItemText key={sdo.id} primary={sdo.type + " " + sdo.name}/>);
-        });
-        return objItems;
     }
 
     // change the selected collection
     handleSelected = (event, colid) => {
         this.setState({selectedColid: colid});
         // find the collection info
-        let thisCol = this.state.collectionList.find(col => col.id === colid);
-        if (thisCol) this.dataObjectList(thisCol);
-        localStorage.setItem('collectionSelected', JSON.stringify(thisCol));
+        let thisColInfo = this.state.collectionList.find(col => col.id === colid);
+        this.setState({info: thisColInfo});
+        localStorage.setItem('collectionSelected', JSON.stringify(thisColInfo));
+    };
+
+    showCollectionInfo() {
+        if (this.state.selectedColid) {
+            let canread = this.state.info.can_read ? 'yes' : 'no';
+            let canwrite = this.state.info.can_write ? 'yes' : 'no';
+            return <Table style={{marginLeft: 8}}>
+                <TableBody>
+                    <TableRow key="Title">
+                        <TableCell>Title</TableCell>
+                        <TableCell>{this.state.info.title}</TableCell>
+                    </TableRow>
+                    <TableRow key="Id">
+                        <TableCell>Id</TableCell>
+                        <TableCell>{this.state.info.id}</TableCell>
+                    </TableRow>
+                    <TableRow key="Description">
+                        <TableCell>Description</TableCell>
+                        <TableCell>{this.state.info.description}</TableCell>
+                    </TableRow>
+                    <TableRow key="CanRead">
+                        <TableCell>Can be read</TableCell>
+                        <TableCell>{canread}</TableCell>
+                    </TableRow>
+                    <TableRow key="CanWrite">
+                        <TableCell>Can write to</TableCell>
+                        <TableCell>{canwrite}</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>;
+        } else {
+            return <div> no collection information </div>
+        }
     };
 
     render() {
         return (
             <Grid container className={this.props.root}>
 
-                <Grid item xs={3}>
+                <Grid item xs={4}>
                     <Typography type="body1">Collections list</Typography>
                     <RadioGroup
                         style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between'}}
@@ -161,12 +147,11 @@ export class CollectionsPage extends Component {
                         onChange={this.handleSelected}>
                         {this.colsAsFormLabels()}
                     </RadioGroup>
-
                 </Grid>
 
-                <Grid item xs={9}>
-                    <Typography type="body1">Objects list</Typography>
-                    <List> {this.objsAsFormLabels()} </List>
+                <Grid item xs={8}>
+                    <Typography type="body1">Collection information</Typography>
+                    {this.showCollectionInfo()}
                 </Grid>
 
                 <div style={{marginLeft: 400, marginTop: 40}}>
